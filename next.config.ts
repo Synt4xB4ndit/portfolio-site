a390b9add284c31b00fb4608a7ca50e292adf76a
Nic Amaral <nicamaral@Nics-Laptop.local>
2025-06-02 00:49:35 -0400

    removed slash as resulted in error

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/portfolio-site/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/portfolio-site' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio-site/' : '',
   images: {
     unoptimized: true, // Disable Image Optimization for static export

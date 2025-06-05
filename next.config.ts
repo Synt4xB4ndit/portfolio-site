adeacbbd4ec3d8aa3030a8cf78016658a7b49d62
Nic Amaral <nicamaral@Nics-Laptop.local>
2025-06-05 00:18:25 -0400

    update css

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/portfolio-site' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio-site/' : '',
+  basePath: /*process.env.NODE_ENV === 'production' ? '/portfolio-site' : */ '',
+  assetPrefix: /* process.env.NODE_ENV === 'production' ? '/portfolio-site/' : */ '',
   images: {
     unoptimized: true, // Disable Image Optimization for static export
     remotePatterns: [

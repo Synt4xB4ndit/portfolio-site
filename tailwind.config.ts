00c2ee692cd9df0d3267fa2f87772a1e3400bed0
Nic Amaral <nicamaral@Nics-Laptop.local>
2025-06-04 23:51:26 -0400

    fixing css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,33 @@
+//import type { Config } from "tailwindcss";
+
+//export default {
+  //content: [
+    //"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    //"./components/**/*.{js,ts,jsx,tsx,mdx}",
+    //"./app/**/*.{js,ts,jsx,tsx,mdx}",
+  //],
+  //theme: {
+  //  extend: {
+    //  colors: {
+     //   background: "var(--background)",
+      //  foreground: "var(--foreground)",
+    //  },
+   // },
+ // },
+ // plugins: [],
+//} satisfies Config;
+
 import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/page.tsx",
+    "./app/about/page.tsx",
+    "./app/contact/page.tsx",
+    "./app/layout.tsx",
+    "./components/ImageLink.tsx",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
